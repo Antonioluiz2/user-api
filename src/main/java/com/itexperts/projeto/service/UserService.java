a4b62ced77bb3a674b7520a4b0f1c51aab6da388
Luiz <antonioluizti@hotmail.com>
2022-03-05 10:52:45 -0300

    10

--- src/main/java/com/itexperts/projeto/service/UserService.java
+++ src/main/java/com/itexperts/projeto/service/UserService.java
@@ -1,5 +1,7 @@
 package com.itexperts.projeto.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +15,20 @@ public class UserService {
 	private UserRepository userRepository;
 	
 	public User create(User user) { 
-		
+		//return userRepository.save(user);
 		User u= new User();
 		u=userRepository.save(user);
+		return u;
+		 		
+	}
+	public User update(User user, Long id) { 
+		Optional<User>userReturned= userRepository.findById(id);
+		userReturned.orElseThrow(()-> new RuntimeException("User Not Found"));
+		userReturned.get().setLastName(user.getName());
+		userReturned.get().setLastName(user.getLastName());
+		User u= new User();
+		
+		u=userRepository.save(userReturned.get());
 		return u;
 		 		
 	}
